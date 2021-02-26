7524267c9cd9f594a53820fceff403692402e2d4
siegfriedjahnel <info@sj-sam.de>
2021-02-26 21:51:54 +0100

    comp

--- app.js
+++ app.js
@@ -9,25 +9,45 @@
 
 const tabelBody = document.getElementById("tableBody");
 const dataList = document.getElementById("dataList");
+const btnAddLocation = document.getElementById("btnAddLocation");
+
+
 alleOrte.forEach(element=>{
   dataList.innerHTML += `<option id="${element.attributes.AdmUnitId}" value="${element.attributes.county}--${element.attributes.AdmUnitId}" \>`;
 })
-let myLocations = [];
+let myLocations = [];//array of objects of locations
+let myObjects ={}; //object of locations
+
+init();
+
+async function getDatafromRKI(){
+  let query="";
+  myLocations.forEach(function(element){
+    query = query + "AdmUnitId="+element.AdmUnitId + " OR ";
+  })
+  query = query.substring(0, query.length-4);//remove the last OR
+  console.log(query);
+  let uri = `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?f=json&where=(${query})&outFields=*`;
+  const response = await fetch(uri);
+  const json = await response.json();
+  const data = await json.features;
+  return data;//data = array of results
+}
 
 function init(){
   if(localStorage.getItem('myLocations')){
     myLocations = localStorage.getItem('myLocations');
     myLocations = JSON.parse(myLocations);
   }
-  tabelBody.innerHTML="";
-  drawTable();
+  tableBody.innerHTML="";
+  getDatafromRKI()
+  .then(function(data){
+    drawTable(data);
+  })
 }
 
-const btnRemoveLocation = document.getElementById("btnRemoveLocation");
-btnRemoveLocation.addEventListener('click', function(){
-  removeLocation(0);
-})
-const btnAddLocation = document.getElementById("btnAddLocation");
+
+
 btnAddLocation.addEventListener('click',function(){
   let tf1 = document.getElementById("tf1");
   if(tf1.value!=''){
@@ -37,30 +57,51 @@ btnAddLocation.addEventListener('click',function(){
     tf1.value="";
   }
 })
-init();
-function drawTable(){
-  myLocations.forEach(function(element){
-    //getDatafromRKI(AdminUnitId)
+
+function drawTable(data){
+  data.forEach(function(element){
+    let ea = element.attributes;
+    let placeName = getCountyByAdmUniId(ea.AdmUnitId);
     let tr = document.createElement("tr");
-    tr.innerHTML = `<tr>${element.AdmUnitId} -- ${element.county}</tr>`;
+    tr.innerHTML = `<tr>
+    <td>${placeName}</td>
+    <td> ${ea.AnzFall}</td>
+    <td>${ea.AnzFallNeu}</td>
+    <td>${ea.AnzFallNeu}</td>
+    <td>${ea.AnzFallNeu}</td>
+    <td>${ea.Inz7T}</td>
+    <td><button onClick="removeLocation(${ea.AdmUnitId})">X</button>
+    </tr>`;
     tableBody.appendChild(tr);
   })
 }
 
 function addNewLocation(AdmUnitId, county){
-  //insert new location
   let location = new Object;
   location.AdmUnitId = AdmUnitId;
   location.county = county;
-  console.log(location);
   myLocations.push(location);
-  console.log(myLocations);
   localStorage.setItem('myLocations', JSON.stringify(myLocations));
   init();
 }
 
-function removeLocation(index){
-  myLocations.splice(index, 1);
-  localStorage.setItem('myLocations', JSON.stringify(myLocations));
-  init();
+function removeLocation(id){
+  myLocations.forEach(element =>{
+    if (element.AdmUnitId == id) {
+      let index = myLocations.indexOf(element);
+      myLocations.splice(index, 1);
+      localStorage.setItem('myLocations', JSON.stringify(myLocations));
+      init();
+    }
+  })
+}
+
+function getCountyByAdmUniId(id) {
+  let name = "";
+  myLocations.forEach(element => {
+    if (element.AdmUnitId == id) {
+      name = element.county;
+    }
+  })
+  return name;
 }
