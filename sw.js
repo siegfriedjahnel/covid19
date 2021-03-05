2c5aa98ebc8e9c52ba66494c739158c42d078ca9
siegfriedjahnel <info@sj-sam.de>
2021-03-05 08:33:30 +0100

    sw-modified

---
+++ sw.js
@@ -0,0 +1,23 @@
+
+  //service worker
+  // 1. get from web
+  // 2. update cache
+  // 3. get from cache
+  //---------------------------
+
+
+
+ this.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      return caches.open("covid19-dynamic-v1").then(function(cache) {
+        return cache.put(event.request, response.clone()).then(function() {
+          return response
+        })
+      })
+    }).catch(function() {
+      return caches.match(event.request)
+    })
+  )
+})
+ 
